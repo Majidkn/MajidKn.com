bb9829b1de336f27cfecea4c8eb65da4cb1a4e2e
Majid Nayyeri <nayyeri@viratech.ir>
2017-08-06 22:35:49 +0430

    add discuss

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,22 @@ $('.fa-angle-down').click(function() {
         scrollTop: $(".post").offset().top
     }, 1000);
 })
+// <script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*var location = window.location;
+var disqus_config = function () {
+this.page.url = window.location;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = location.split('/')[location.split('/').length-1]; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+this.page.title = this.page.identifier;
+};
+*/
+$(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://majidkn.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+});
+// </script>
