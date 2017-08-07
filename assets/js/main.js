260632b9567e1c92c92e3d4c804fc2c6d2ce1bbf
Majid Kaffash Nayyeri <majidrobo@gmail.com>
2017-08-08 02:46:59 +0430

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,7 +25,7 @@ this.page.title = this.page.identifier;
 */
 $(function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
-s.src = 'https://majidkn.disqus.com/embed.js';
+s.src = 'http://majidkn.com/assets/js/disqus.js';
 s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 });
